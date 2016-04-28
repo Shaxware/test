a8e74de0d79d379795aa9ac733d85dbe41ecbeef
Junichi MORI <junichi.mori@shaxware.com>
2016-04-28 16:29:50 +0900

    test3

--- JenkinsTest/JenkinsTest/Controllers/HomeController.cs
+++ JenkinsTest/JenkinsTest/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace JenkinsTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Jenkins Test";
+            ViewBag.Message = "Jenkins Test3";
 
             return View();
         }
