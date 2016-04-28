79a7006d09ab0c1debc5d5805596399cee2a83ec
Junichi MORI <junichi.mori@shaxware.com>
2016-04-28 11:14:03 +0900

    NUnit対応

---
+++ JenkinsTest/JenkinsTest/Models/ModelTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace JenkinsTest.Models
+{
+    [TestFixture(Description="JenkinsのNUnit動作テストを行います")]
+    public class ModelTest
+    {
+        [Test(Description="OKのケース")]
+        public void TestOK()
+        {
+        }
+
+        [Test(Description ="NGのケース")]
+        public void TestNG()
+        {
+            Assert.Fail();
+        }
+    }
+}
